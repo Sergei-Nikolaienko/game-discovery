8f963495561d55a9a7415bfd1ea702985e8aa022
Sergei Nikolaienko <serjexdeath@gmail.com>
2023-04-08 20:24:24 +0300

    Built a navbar, images imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Grid, GridItem, Show } from '@chakra-ui/react';
+import Navbar from './components/Navbar';
 import './App.css'
 
 function App() {
@@ -10,7 +11,9 @@ function App() {
       lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area='nav' bg='coral'>Nav</GridItem>
+      <GridItem area='nav'>
+        <Navbar />
+      </GridItem>
       <Show above='lg'>
         <GridItem area='aside' bg='gold'>Aside</GridItem>
       </Show>
